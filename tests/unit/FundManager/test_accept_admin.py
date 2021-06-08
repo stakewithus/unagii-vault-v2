caddcf4572661230cf74f07e080f3db482d3640e
t4sk <tsk.nakamura@gmail.com>
2021-06-08 16:34:29 +0900

    fund manager accept admin

--- tests/unit/FundManager/test_accept_admin.py
+++ tests/unit/FundManager/test_accept_admin.py
@@ -2,14 +2,14 @@ import brownie
 import pytest
 
 
-def test_accept_admin(vault, admin, user):
-    vault.setNextAdmin(user, {"from": admin})
+def test_accept_admin(fundManager, admin, user):
+    fundManager.setNextAdmin(user, {"from": admin})
 
     # not next admin
     with brownie.reverts("!next admin"):
-        vault.acceptAdmin({"from": admin})
+        fundManager.acceptAdmin({"from": admin})
 
-    tx = vault.acceptAdmin({"from": user})
-    assert vault.admin() == user
+    tx = fundManager.acceptAdmin({"from": user})
+    assert fundManager.admin() == user
     assert len(tx.events) == 1
     assert tx.events["AcceptAdmin"].values() == [user]
