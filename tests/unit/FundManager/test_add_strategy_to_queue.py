4fcb563aace4886c5193cf5f1d628b59f957cab5
t4sk <tsk.nakamura@gmail.com>
2021-06-09 14:36:26 +0900

    fund manager: fix add strategy to queeu

--- tests/unit/FundManager/test_add_strategy_to_queue.py
+++ tests/unit/FundManager/test_add_strategy_to_queue.py
@@ -7,23 +7,27 @@ def test_add_strategy_to_queue(fundManager, admin, keeper, testStrategy, user):
 
     # revert if not authorized
     with brownie.reverts("!auth"):
-        fundManager.addStrategyToQueue(strategy, 0, {"from": user})
+        fundManager.addStrategyToQueue(strategy, 0, 0, 0, {"from": user})
 
     # revert if not approved
     with brownie.reverts("!approved"):
-        fundManager.addStrategyToQueue(strategy, 0, {"from": keeper})
+        fundManager.addStrategyToQueue(strategy, 0, 0, 0, {"from": keeper})
 
     fundManager.approveStrategy(strategy, {"from": admin})
 
     # revert if debt ratio > max
     with brownie.reverts("ratio > max"):
-        fundManager.addStrategyToQueue(strategy, 100001, {"from": keeper})
+        fundManager.addStrategyToQueue(strategy, 100001, 0, 0, {"from": keeper})
+
+    # revert if min borrow > max borrow
+    with brownie.reverts("min borrow > max borrow"):
+        fundManager.addStrategyToQueue(strategy, 0, 2, 1, {"from": keeper})
 
     def snapshot():
         return {"totalDebtRatio": fundManager.totalDebtRatio()}
 
     before = snapshot()
-    tx = fundManager.addStrategyToQueue(strategy, 123, {"from": keeper})
+    tx = fundManager.addStrategyToQueue(strategy, 123, 11, 22, {"from": keeper})
     after = snapshot()
 
     strat = fundManager.strategies(strategy)
@@ -31,6 +35,8 @@ def test_add_strategy_to_queue(fundManager, admin, keeper, testStrategy, user):
     assert strat["active"]
     assert strat["activated"]
     assert strat["debtRatio"] == 123
+    assert strat["minBorrow"] == 11
+    assert strat["maxBorrow"] == 22
 
     assert after["totalDebtRatio"] == before["totalDebtRatio"] + 123
     assert fundManager.queue(0) == strategy
@@ -39,4 +45,4 @@ def test_add_strategy_to_queue(fundManager, admin, keeper, testStrategy, user):
 
     # revert if active
     with brownie.reverts("active"):
-        fundManager.addStrategyToQueue(strategy, 0, {"from": keeper})
+        fundManager.addStrategyToQueue(strategy, 0, 0, 0, {"from": keeper})
