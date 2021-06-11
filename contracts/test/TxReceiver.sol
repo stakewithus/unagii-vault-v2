94fc5354375f0d0a8a6a5263b0e9e12d56edaae7
t4sk <tsk.nakamura@gmail.com>
2021-06-11 16:09:41 +0900

    rename TestTimeLock to TxReceiver

--- contracts/test/TxReceiver.sol
+++ contracts/test/TxReceiver.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8;
 
-contract TestTimeLock {
+contract TxReceiver {
     // test helper
     bytes public data;
     uint public value;
