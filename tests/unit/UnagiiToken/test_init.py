1f2632122b12a3932a3e9e56af790ce1a2714b51
t4sk <tsk.nakamura@gmail.com>
2021-06-11 16:14:17 +0900

    uToken: test init

--- tests/unit/UnagiiToken/test_init.py
+++ tests/unit/UnagiiToken/test_init.py
@@ -1,9 +1,8 @@
 import brownie
 
 
-def test_init(uToken, token, admin, minter):
-    assert uToken.admin() == admin
-    assert uToken.minter() == minter
+def test_init(uToken, token, admin):
+    assert uToken.timeLock() == admin
     assert uToken.token() == token.address
     assert uToken.name() == "unagii_" + token.name() + "_v2"
     assert uToken.symbol() == "u" + token.symbol() + "v2"
