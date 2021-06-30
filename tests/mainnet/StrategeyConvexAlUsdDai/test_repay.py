b4f8b6d62ff391a74acade9fa9e12cd0a3abb9cd
t4sk <tsk.nakamura@gmail.com>
2021-07-01 08:41:43 +0900

    setup strategy in fixture

--- tests/mainnet/StrategeyConvexAlUsdDai/test_repay.py
+++ tests/mainnet/StrategeyConvexAlUsdDai/test_repay.py
@@ -3,17 +3,11 @@ from brownie import StrategyConvexAlUsdDai
 import pytest
 
 
-def test_repay(daiFundManager, admin, treasury, dai, dai_whale):
+def test_repay(strategy, daiFundManager, admin, treasury, dai, dai_whale):
     token = dai
     whale = dai_whale
 
     fundManager = daiFundManager
-    timeLock = fundManager.timeLock()
-
-    strategy = StrategyConvexAlUsdDai.deploy(fundManager, treasury, {"from": admin})
-
-    fundManager.approveStrategy(strategy, {"from": timeLock})
-    fundManager.addStrategyToQueue(strategy, 1, 0, 2 ** 256 - 1, {"from": admin})
 
     amount = 10 ** 18
     token.transfer(fundManager, amount, {"from": whale})
