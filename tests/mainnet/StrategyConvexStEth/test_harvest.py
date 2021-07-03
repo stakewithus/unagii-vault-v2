5a2f7c50173edb8f27d1c3d2310a7b0c15b13fa0
t4sk <tsk.nakamura@gmail.com>
2021-07-03 14:17:12 +0900

    StrategyConvexStEth: test

---
+++ tests/mainnet/StrategyConvexStEth/test_harvest.py
@@ -0,0 +1,41 @@
+import brownie
+import pytest
+
+
+def test_harvest(strategy, ethFundManager, admin, eth_whale):
+    whale = eth_whale
+
+    fundManager = ethFundManager
+
+    # deposit into fund manager
+    deposit_amount = 10 ** 18
+    eth_whale.transfer(fundManager, deposit_amount)
+
+    # transfer to strategy
+    strategy.deposit(2 ** 256 - 1, 1, {"from": admin})
+
+    def snapshot():
+        return {
+            "eth": {
+                "strategy": strategy.balance(),
+                "fundManager": fundManager.balance(),
+            },
+            "strategy": {"totalAssets": strategy.totalAssets()},
+            "fundManager": {"debt": fundManager.getDebt(strategy)},
+        }
+
+    # create profit
+    min_profit = 10 ** 18
+    eth_whale.transfer(strategy, min_profit)
+
+    before = snapshot()
+    tx = strategy.harvest(1, 0, 2 ** 256 - 1, {"from": admin})
+    after = snapshot()
+
+    # print(before)
+    # print(after)
+    # for e in tx.events:
+    #     print(e)
+
+    assert after["strategy"]["totalAssets"] >= before["fundManager"]["debt"]
+    assert after["eth"]["fundManager"] >= before["eth"]["fundManager"]
