cb60e235f91ec5e03b551ba26cdce14db47c5a33
t4sk <tsk.nakamura@gmail.com>
2021-07-06 16:25:42 +0900

    StrategyConvexUsdpUsdc tests

---
+++ tests/mainnet/StrategyConvexUsdpUsdc/conftest.py
@@ -0,0 +1,16 @@
+import brownie
+from brownie import interface, StrategyConvexUsdpUsdc
+import pytest
+
+
+@pytest.fixture(scope="session")
+def strategy(usdcFundManager, admin, treasury):
+    fundManager = usdcFundManager
+    timeLock = fundManager.timeLock()
+
+    strategy = StrategyConvexUsdpUsdc.deploy(fundManager, treasury, {"from": admin})
+
+    fundManager.approveStrategy(strategy, {"from": timeLock})
+    fundManager.addStrategyToQueue(strategy, 1, 0, 2 ** 256 - 1, {"from": admin})
+
+    yield strategy
