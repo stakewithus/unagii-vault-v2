237785c3c54fec3801eb07293f5d69a8023ea3d9
t4sk <tsk.nakamura@gmail.com>
2021-07-20 11:18:02 +0900

    version 0.1.1

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.4;
 
+// version 0.1.1
+
 contract TimeLock {
     enum State {
         Queued,
--- contracts/strategies/StrategyCompLevDai.sol
+++ contracts/strategies/StrategyCompLevDai.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.7.6;
+// version 0.1.1
 
 import "./StrategyCompLev.sol";
 
