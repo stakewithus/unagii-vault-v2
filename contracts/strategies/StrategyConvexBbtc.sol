48d887f030d0b0a986917fc62ff46035c1ebe2c5
t4sk <tsk.nakamura@gmail.com>
2021-07-20 10:13:35 +0900

    fix SCB-03 | Usage of literal for arrays' lengths

--- contracts/strategies/StrategyConvexBbtc.sol
+++ contracts/strategies/StrategyConvexBbtc.sol
@@ -16,15 +16,16 @@ contract StrategyConvexBbtc is Strategy {
     // UNISWAP = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     // SUSHISWAP = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    uint private constant NUM_REWARDS = 2;
     // address of DEX (uniswap or sushiswap) to use for selling reward tokens
     // CRV, CVX
-    address[2] public dex;
+    address[NUM_REWARDS] public dex;
 
     address private constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
     address private constant CVX = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
 
     // Solc 0.7 cannot create constant arrays
-    address[2] private REWARDS = [CRV, CVX];
+    address[NUM_REWARDS] private REWARDS = [CRV, CVX];
 
     // Convex //
     Booster private constant BOOSTER =
@@ -311,7 +312,7 @@ contract StrategyConvexBbtc is Strategy {
             "get reward failed"
         );
 
-        for (uint i = 0; i < REWARDS.length; i++) {
+        for (uint i = 0; i < NUM_REWARDS; i++) {
             uint rewardBal = IERC20(REWARDS[i]).balanceOf(address(this));
             if (rewardBal > 0) {
                 // swap may fail if rewards are too small
@@ -418,7 +419,7 @@ contract StrategyConvexBbtc is Strategy {
     */
     function sweep(address _token) external override onlyAuthorized {
         require(_token != address(token), "protected token");
-        for (uint i = 0; i < REWARDS.length; i++) {
+        for (uint i = 0; i < NUM_REWARDS; i++) {
             require(_token != REWARDS[i], "protected token");
         }
         IERC20(_token).safeTransfer(admin, IERC20(_token).balanceOf(address(this)));
