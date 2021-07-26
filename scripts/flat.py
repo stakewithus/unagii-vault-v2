9cdacdf61185e3d74bccbdeedac89f9e33fc88e8
t4sk <tsk.nakamura@gmail.com>
2021-07-26 12:11:42 +0900

    deploy test strategies

--- scripts/flat.py
+++ scripts/flat.py
@@ -1,9 +1,9 @@
 import brownie
-from brownie import StrategyConvexBbtcWbtc
+from brownie import StrategyEthTest
 
 
 def main():
-    my_contract = StrategyConvexBbtcWbtc
+    my_contract = StrategyEthTest
 
     flattened_source = ""
     for name in my_contract._build["dependencies"]:
