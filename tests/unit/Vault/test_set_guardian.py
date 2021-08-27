20dd498699f515b29560dc78088fd6529528920d
t4sk <tsk.nakamura@gmail.com>
2021-08-27 16:04:51 +0900

    vault: test set guardian

--- tests/unit/Vault/test_set_guardian.py
+++ tests/unit/Vault/test_set_guardian.py
@@ -1,25 +1,20 @@
 import brownie
-import pytest
 
 
-@pytest.fixture(scope="function", autouse=True)
-def setup(fn_isolation):
-    pass
-
-
-def test_set_guardian_time_lock(vault, user):
+def test_set_guardian(chain, vault, admin, user):
     timeLock = vault.timeLock()
 
-    tx = vault.setGuardian(user, {"from": timeLock})
-    assert vault.guardian() == user
-    assert tx.events["SetGuardian"].values() == [user]
+    with brownie.reverts("!auth"):
+        vault.setGuardian(user, {"from": user})
 
-
-def test_set_guardian_admin(vault, admin, user):
+    # admin can call
     vault.setGuardian(user, {"from": admin})
     assert vault.guardian() == user
 
+    chain.undo()
+    assert vault.guardian() != user
 
-def test_set_guardian_not_auth(vault, user):
-    with brownie.reverts("!auth"):
-        vault.setGuardian(user, {"from": user})
+    # time lock can call
+    tx = vault.setGuardian(user, {"from": timeLock})
+    assert vault.guardian() == user
+    assert tx.events["SetGuardian"].values() == [user]
