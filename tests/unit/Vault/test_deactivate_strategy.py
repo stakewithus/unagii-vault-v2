269a1b65370a6559d03009f935bfa63d64a9e44b
t4sk <tsk.nakamura@gmail.com>
2021-08-30 10:54:21 +0900

    Vault: test deactivate strategy

---
+++ tests/unit/Vault/test_deactivate_strategy.py
@@ -0,0 +1,36 @@
+import brownie
+from brownie import ZERO_ADDRESS
+import pytest
+
+
+def test_deactivate_strategy(vault, admin, testStrategy, user):
+    strategy = testStrategy
+    timeLock = vault.timeLock()
+
+    vault.approveStrategy(strategy, {"from": timeLock})
+    vault.activateStrategy(strategy, 123, {"from": admin})
+
+    # revert if not authorized
+    with brownie.reverts("!auth"):
+        vault.deactivateStrategy(strategy, {"from": user})
+
+    def snapshot():
+        return {"totalDebtRatio": vault.totalDebtRatio()}
+
+    before = snapshot()
+    tx = vault.deactivateStrategy(strategy, {"from": admin})
+    after = snapshot()
+
+    strat = vault.strategies(strategy)
+
+    assert not strat["active"]
+    assert strat["debtRatio"] == 0
+
+    assert after["totalDebtRatio"] == before["totalDebtRatio"] - 123
+    assert vault.queue(0) == ZERO_ADDRESS
+
+    assert tx.events["DeactivateStrategy"].values() == [strategy]
+
+    # revert if not active
+    with brownie.reverts("!active"):
+        vault.deactivateStrategy(strategy, {"from": admin})
