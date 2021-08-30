3b4c69072d8c984308e795c696e787d0d2cfdd6a
t4sk <tsk.nakamura@gmail.com>
2021-08-30 10:36:24 +0900

    vault: test deactivate strategy

---
+++ tests/unit/Vault/test_revoke_strategy.py
@@ -0,0 +1,30 @@
+import brownie
+
+
+def test_revoke_strategy(vault, admin, testStrategy, user):
+    strategy = testStrategy
+    timeLock = vault.timeLock()
+
+    # revert if not authorized
+    with brownie.reverts("!auth"):
+        vault.revokeStrategy(strategy, {"from": user})
+
+    # revert if not approved
+    with brownie.reverts("!approved"):
+        vault.revokeStrategy(strategy, {"from": admin})
+
+    vault.approveStrategy(strategy, {"from": timeLock})
+
+    # revert if active
+    vault.activateStrategy(strategy, 1, {"from": admin})
+
+    with brownie.reverts("active"):
+        vault.revokeStrategy(strategy, {"from": admin})
+
+    vault.deactivateStrategy(strategy, {"from": admin})
+
+    tx = vault.revokeStrategy(strategy, {"from": admin})
+    strat = vault.strategies(strategy)
+
+    assert not strat["approved"]
+    assert tx.events["RevokeStrategy"].values() == [strategy]
