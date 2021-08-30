57dadd7e90acfc9d7ebe2f82c3aa62a2425b5a96
t4sk <tsk.nakamura@gmail.com>
2021-08-30 13:21:34 +0900

    vault: test set debt ratios

---
+++ tests/unit/Vault/test_set_debt_ratios.py
@@ -0,0 +1,47 @@
+import brownie
+from brownie import ZERO_ADDRESS
+from brownie.test import given, strategy
+
+# max queue
+N = 20
+
+
+@given(
+    # number of active strategies
+    k=strategy("uint256", min_value=0, max_value=N),
+    # random numbers for debt ratios
+    rands=strategy("uint256[]", min_value=0, max_value=100, min_length=N, max_length=N),
+)
+def test_set_debt_ratios(vault, admin, TestStrategy, token, user, k, rands):
+    timeLock = vault.timeLock()
+
+    # not authorized
+    with brownie.reverts("!auth"):
+        debtRatios = [0 for i in range(N)]
+        vault.setDebtRatios(debtRatios, {"from": user})
+
+    strats = []
+    for i in range(k):
+        strat = TestStrategy.deploy(vault, token, {"from": admin})
+        vault.approveStrategy(strat, {"from": timeLock})
+        vault.activateStrategy(strat, 1, {"from": admin})
+        strats.append(strat.address)
+
+    # test total debt ratio > max
+    if k > 0:
+        with brownie.reverts("total > max"):
+            debtRatios = [0 for i in range(N)]
+            debtRatios[0] = 10001
+            vault.setDebtRatios(debtRatios, {"from": admin})
+
+    debtRatios = [rands[i] if i < k else 0 for i in range(N)]
+    totalDebtRatio = sum(debtRatios)
+
+    tx = vault.setDebtRatios(debtRatios, {"from": admin})
+
+    assert vault.totalDebtRatio() == totalDebtRatio
+    assert tx.events["SetDebtRatios"].values() == [debtRatios]
+
+    for i in range(N):
+        addr = vault.queue(i)
+        assert vault.strategies(addr)["debtRatio"] == debtRatios[i]
