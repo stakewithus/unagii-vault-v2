2e861c605ef37498f8780602ca6e2cfc7610f540
t4sk <tsk.nakamura@gmail.com>
2021-09-08 14:32:03 +0900

    v2 strategy test migrate

---
+++ contracts/v2/V2StrategyTest.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.6;
+
+import "./V2Strategy.sol";
+
+interface ITestToken {
+    function burn(address, uint) external;
+}
+
+contract V2StrategyTest is V2Strategy {
+    using SafeERC20 for IERC20;
+
+    constructor(
+        address _token,
+        address _fundManager,
+        address _treasury
+    ) V2Strategy(_token, _fundManager, _treasury) {}
+
+    function _totalAssets() internal view returns (uint) {
+        return token.balanceOf(address(this));
+    }
+
+    function totalAssets() external view override returns (uint) {
+        return _totalAssets();
+    }
+
+    function deposit(uint _amount, uint _min) external override onlyAuthorized {
+        uint borrowed = fundManager.borrow(_amount);
+        require(borrowed >= _min, "borrowed < min");
+        emit Deposit(_amount, borrowed);
+    }
+
+    function withdraw(uint _amount) external override onlyFundManager returns (uint) {
+        uint amount = _amount;
+        uint bal = token.balanceOf(address(this));
+        if (bal < amount) {
+            amount = bal;
+        }
+
+        uint loss = 0;
+        uint debt = fundManager.getDebt(address(this));
+        uint total = _totalAssets();
+        if (debt > total) {
+            loss = debt - total;
+        }
+
+        token.safeTransfer(msg.sender, amount);
+
+        emit Withdraw(_amount, amount, loss);
+
+        return loss;
+    }
+
+    function repay(uint _amount, uint _min) external override onlyAuthorized {
+        uint repaid = fundManager.repay(_amount);
+        require(repaid >= _min, "repaid < min");
+        emit Repay(_amount, repaid);
+    }
+
+    function _claimRewards(uint) internal {
+        emit ClaimRewards(0);
+    }
+
+    function claimRewards(uint _minProfit) external override onlyAuthorized {
+        _claimRewards(_minProfit);
+    }
+
+    function _skim() internal {
+        uint total = _totalAssets();
+        uint debt = fundManager.getDebt(address(this));
+        emit Skim(total, debt, 0);
+    }
+
+    function skim() external override onlyAuthorized {
+        _skim();
+    }
+
+    function _report(uint, uint) internal {
+        uint total = _totalAssets();
+
+        uint gain = 0;
+        uint loss = 0;
+        uint free = 0;
+        uint debt = fundManager.getDebt(address(this));
+
+        if (total > debt) {
+            gain = total - debt;
+
+            free = token.balanceOf(address(this));
+            if (gain > free) {
+                gain = free;
+            }
+        } else {
+            loss = debt - total;
+        }
+
+        if (gain > 0 || loss > 0) {
+            fundManager.report(gain, loss);
+        }
+
+        emit Report(gain, loss, free, total, debt);
+    }
+
+    function report(uint _minTotal, uint _maxTotal) external override onlyAuthorized {
+        _report(_minTotal, _maxTotal);
+    }
+
+    function harvest(
+        uint _minProfit,
+        uint _minTotal,
+        uint _maxTotal
+    ) external override onlyAuthorized {
+        _claimRewards(_minProfit);
+        _skim();
+        _report(_minTotal, _maxTotal);
+    }
+
+    function migrate(address _strategy) external override onlyAuthorized {
+        token.transfer(_strategy, token.balanceOf(address(this)));
+    }
+
+    function sweep(address _token) external override {}
+}
---
+++ tests/integration/test_migrate_v2_v3.py
@@ -0,0 +1,205 @@
+import brownie
+from brownie import (
+    ZERO_ADDRESS,
+    V2Vault,
+    V2FundManager,
+    V2StrategyTest,
+    StrategyMigrate,
+    Vault,
+)
+import pytest
+
+DELAY = 24 * 3600
+N = 3  # number of active strategies
+
+
+@pytest.fixture(scope="module")
+def v2(token, uToken, admin):
+    yield V2Vault.deploy(token, uToken, ZERO_ADDRESS, ZERO_ADDRESS, {"from": admin})
+
+
+@pytest.fixture(scope="module")
+def fundManager(token, admin):
+    yield V2FundManager.deploy(
+        token, ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS, {"from": admin}
+    )
+
+
+@pytest.fixture(scope="module")
+def v2_setup(chain, v2, fundManager, token, uToken, timeLock, admin, user):
+    uToken.setMinter(v2, {"from": admin})
+
+    # uToken set time lock
+    uToken.setNextTimeLock(timeLock, {"from": admin})
+    uToken.acceptTimeLock({"from": timeLock})
+
+    fundManager.setVault(v2, {"from": admin})
+    fundManager.initialize({"from": admin})
+
+    # fund manager set time lock
+    fundManager.setNextTimeLock(timeLock, {"from": admin})
+    fundManager.acceptTimeLock({"from": timeLock})
+
+    v2.setFundManager(fundManager, {"from": admin})
+
+    v2.setPause(False, {"from": admin})
+    v2.setDepositLimit(2 ** 256 - 1, {"from": admin})
+    v2.initialize({"from": admin})
+
+    v2.setNextTimeLock(timeLock, {"from": admin})
+    v2.acceptTimeLock({"from": timeLock})
+
+    # activate strategies
+    strats = []
+    for i in range(N):
+        strat = V2StrategyTest.deploy(token, fundManager, admin, {"from": admin})
+
+        # set time lock
+        strat.setNextTimeLock(timeLock, {"from": admin})
+        strat.acceptTimeLock({"from": timeLock})
+
+        # this will be time locked on mainnet
+        fundManager.approveStrategy(strat, {"from": timeLock})
+
+        fundManager.addStrategyToQueue(strat, 100, 0, 2 ** 256 - 1, {"from": admin})
+        strats.append(strat)
+
+    # deposit
+    token.mint(user, 1000)
+    token.approve(v2, 1000, {"from": user})
+    v2.deposit(1000, 1000, {"from": user})
+
+    # deposit strategies
+    fundManager.borrowFromVault(2 ** 256 - 1, 1, {"from": admin})
+
+    for strat in strats:
+        strat.deposit(2 ** 256 - 1, 1, {"from": admin})
+
+
+@pytest.fixture(scope="module")
+def v3(token, uToken, admin):
+    yield Vault.deploy(token, uToken, ZERO_ADDRESS, ZERO_ADDRESS, {"from": admin})
+
+
+@pytest.fixture(scope="module")
+def v3_setup(chain, timeLock, v3, admin):
+    v3.setNextTimeLock(timeLock, {"from": admin})
+
+    data = v3.acceptTimeLock.encode_input()
+    tx = timeLock.queue(v3, 0, data, DELAY, 0, {"from": admin})
+    eta = tx.timestamp + DELAY
+    chain.sleep(DELAY)
+    timeLock.execute(v3, 0, data, eta, 0, {"from": admin})
+
+
+@pytest.fixture(scope="module")
+def strategyMigrate(chain, token, fundManager, v2, v3, admin):
+    yield StrategyMigrate.deploy(token, fundManager, admin, v2, v3, {"from": admin})
+
+
+def test_migrate_v2_v3(
+    chain,
+    token,
+    uToken,
+    admin,
+    timeLock,
+    v2_setup,
+    v2,
+    fundManager,
+    v3,
+    v3_setup,
+    strategyMigrate,
+):
+    assert v2.timeLock() == timeLock
+    assert v2.admin() == admin
+    assert not v2.paused()
+    assert uToken.minter() == v2
+
+    assert fundManager.timeLock() == timeLock
+    assert fundManager.admin() == admin
+
+    assert v3.timeLock() == timeLock
+    assert v3.admin() == admin
+    assert v3.paused()
+
+    # withdraw from strategies
+    for i in range(N):
+        strat = V2StrategyTest.at(fundManager.queue(i))
+        strat.repay(2 ** 256 - 1, 1, {"from": admin})
+
+    # deactivate strategies
+    for i in range(N):
+        strat = V2StrategyTest.at(fundManager.queue(0))
+        fundManager.removeStrategyFromQueue(strat, {"from": admin})
+
+    # set time lock on strategy migrate
+    strategyMigrate.setNextTimeLock(timeLock, {"from": admin})
+    strategyMigrate.acceptTimeLock({"from": timeLock})
+
+    # approve and activate strategy migrate
+    fundManager.approveStrategy(strategyMigrate, {"from": timeLock})
+
+    fundManager.addStrategyToQueue(
+        strategyMigrate, 100, 0, 2 ** 256 - 1, {"from": admin}
+    )
+
+    v2.setMinReserve(0, {"from": admin})
+
+    assert v2.calcMaxBorrow() == token.balanceOf(v2)
+
+    def snapshot():
+        return {
+            "v2": {
+                "totalAssets": v2.totalAssets(),
+                "debt": v2.debt(),
+            },
+            "token": {
+                "v2": token.balanceOf(v2),
+                "v3": token.balanceOf(v3),
+                "fundManager": token.balanceOf(fundManager),
+                "strategyMigrate": token.balanceOf(strategyMigrate),
+            },
+        }
+
+    # time lock migration
+    targets = [
+        fundManager,
+        v2,
+        strategyMigrate,
+        uToken,
+    ]
+    values = [0, 0, 0, 0]
+    data = [
+        fundManager.borrowFromVault.encode_input(2 ** 256 - 1, 1),
+        v2.setPause.encode_input(True),
+        strategyMigrate.migrateToV3.encode_input(),
+        uToken.setMinter.encode_input(v3),
+    ]
+    delays = [DELAY, DELAY, DELAY, DELAY]
+    nonces = [0, 0, 0, 0]
+
+    tx = timeLock.batchQueue(targets, values, data, delays, nonces, {"from": admin})
+    eta = tx.timestamp + DELAY
+
+    chain.sleep(DELAY)
+
+    # execute time lock
+    etas = [eta, eta, eta, eta]
+    before = snapshot()
+    tx = timeLock.batchExecute(targets, values, data, etas, nonces, {"from": admin})
+    after = snapshot()
+
+    assert before["v2"]["totalAssets"] > 0
+    assert before["token"]["v2"] > 0
+    assert before["token"]["fundManager"] > 0
+    assert before["token"]["fundManager"] >= before["v2"]["debt"]
+    assert before["token"]["strategyMigrate"] == 0
+    assert before["token"]["v3"] == 0
+
+    assert after["token"]["v2"] == 0
+    assert after["token"]["fundManager"] == 0
+    assert after["token"]["strategyMigrate"] == 0
+    assert after["token"]["v3"] == before["v2"]["totalAssets"]
+
+    assert v2.paused()
+    assert uToken.minter() == v3
