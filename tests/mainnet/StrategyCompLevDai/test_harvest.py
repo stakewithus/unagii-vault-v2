d692cd8eeb45c1230f80ceffc0e489b84286e557
t4sk <tsk.nakamura@gmail.com>
2021-09-13 14:29:47 +0900

    strategy comp lev test harvest

--- tests/mainnet/StrategyCompLevDai/test_harvest.py
+++ tests/mainnet/StrategyCompLevDai/test_harvest.py
@@ -2,41 +2,43 @@ import brownie
 import pytest
 
 
-def test_harvest(strategy, daiFundManager, admin, dai, dai_whale):
+def test_harvest(strategy, daiVault, admin, treasury, dai, dai_whale):
     token = dai
     whale = dai_whale
-
-    fundManager = daiFundManager
+    vault = daiVault
 
     # deposit into fund manager
     deposit_amount = 100 * 10 ** 18
-    token.transfer(fundManager, deposit_amount, {"from": whale})
+    token.transfer(vault, deposit_amount, {"from": whale})
+
+    calc = vault.calcMaxBorrow(strategy)
+    assert calc > 0
 
     # transfer to strategy
-    strategy.deposit(2 ** 256 - 1, deposit_amount, {"from": admin})
+    strategy.deposit(2 ** 256 - 1, calc, {"from": admin})
 
     def snapshot():
         return {
             "token": {
                 "strategy": token.balanceOf(strategy),
-                "fundManager": token.balanceOf(fundManager),
+                "vault": token.balanceOf(vault),
+                "treasury": token.balanceOf(treasury),
             },
             "strategy": {"totalAssets": strategy.totalAssets()},
-            "fundManager": {"debt": fundManager.getDebt(strategy)},
         }
 
     # create profit
-    min_profit = 10 ** 18
-    token.transfer(strategy, min_profit, {"from": whale})
+    # min_profit = 10 ** 18
+    min_profit = 0
+    # token.transfer(strategy, min_profit, {"from": whale})
 
     before = snapshot()
-    tx = strategy.harvest(1, 0, 2 ** 256 - 1, {"from": admin})
+    strategy.harvest(min_profit, {"from": admin})
     after = snapshot()
 
     print(before)
     print(after)
-    # for e in tx.events:
-    #     print(e)
 
-    assert after["strategy"]["totalAssets"] >= before["fundManager"]["debt"]
-    assert after["token"]["fundManager"] >= before["token"]["fundManager"]
+    assert after["strategy"]["totalAssets"] >= before["strategy"]["totalAssets"]
+    assert after["token"]["strategy"] >= before["token"]["strategy"]
+    assert after["token"]["treasury"] >= before["token"]["treasury"]
