24e22fc71146fb245410cc95c572b0b4dde38c18
t4sk <tsk.nakamura@gmail.com>
2021-09-14 14:54:45 +0900

    EthVault: test set admin

---
+++ tests/unit/EthVault/test_set_admin.py
@@ -0,0 +1,21 @@
+import brownie
+
+
+def test_set_admin(chain, ethVault, admin, user):
+    vault = ethVault
+    timeLock = vault.timeLock()
+
+    with brownie.reverts("!auth"):
+        vault.setAdmin(user, {"from": user})
+
+    # admin can call
+    vault.setAdmin(user, {"from": admin})
+    assert vault.admin() == user
+
+    chain.undo()
+    assert vault.admin() != user
+
+    # time lock can call
+    tx = vault.setAdmin(user, {"from": timeLock})
+    assert vault.admin() == user
+    assert tx.events["SetAdmin"].values() == [user]
