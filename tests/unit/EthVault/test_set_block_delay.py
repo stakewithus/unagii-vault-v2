f0a1a6913bf36731f84f1af1f24054c85c8693a8
t4sk <tsk.nakamura@gmail.com>
2021-09-14 15:00:24 +0900

    EthVault: test set block delay

---
+++ tests/unit/EthVault/test_set_block_delay.py
@@ -0,0 +1,26 @@
+import brownie
+
+
+def test_set_block_delay(ethVault, admin, user):
+    vault = ethVault
+    timeLock = vault.timeLock()
+
+    # not auth
+    with brownie.reverts("!auth"):
+        vault.setBlockDelay(123, {"from": user})
+
+    # delay = 0
+    with brownie.reverts("delay out of range"):
+        vault.setBlockDelay(0, {"from": admin})
+
+    # delay > max
+    with brownie.reverts("delay out of range"):
+        vault.setBlockDelay(1001, {"from": admin})
+
+    # time lock can call
+    vault.setBlockDelay(123, {"from": timeLock})
+    assert vault.blockDelay() == 123
+
+    # admin can call
+    vault.setBlockDelay(321, {"from": admin})
+    assert vault.blockDelay() == 321
