6f7a04931c3ece2e0fc002ff084736d700e21c67
t4sk <tsk.nakamura@gmail.com>
2021-09-14 14:52:34 +0900

    EthVault: test set next time lock

---
+++ tests/unit/EthVault/test_set_next_time_lock.py
@@ -0,0 +1,15 @@
+import brownie
+import pytest
+
+
+def test_set_next_time_lock(ethVault, user):
+    vault = ethVault
+    timeLock = vault.timeLock()
+
+    # not time lock
+    with brownie.reverts("!time lock"):
+        vault.setNextTimeLock(user, {"from": user})
+
+    tx = vault.setNextTimeLock(user, {"from": timeLock})
+    assert vault.nextTimeLock() == user
+    assert tx.events["SetNextTimeLock"].values() == [user]
