230901357247fe4f426c5f42c7f85f25e17038cc
t4sk <tsk.nakamura@gmail.com>
2021-09-15 10:41:24 +0900

    strategy eth - test set treasury

---
+++ tests/unit/StrategyEth/test_set_treasury.py
@@ -0,0 +1,18 @@
+import brownie
+from brownie import ZERO_ADDRESS
+
+
+def test_set_treasury(strategyEthTest, admin, user):
+    strategy = strategyEthTest
+
+    # not auth
+    with brownie.reverts("!auth"):
+        strategy.setTreasury(user, {"from": user})
+
+    # zero address
+    with brownie.reverts("treasury = 0 address"):
+        strategy.setTreasury(ZERO_ADDRESS, {"from": admin})
+
+    tx = strategy.setTreasury(user, {"from": admin})
+    assert strategy.treasury() == user
+    assert tx.events["SetTreasury"].values() == [user]
