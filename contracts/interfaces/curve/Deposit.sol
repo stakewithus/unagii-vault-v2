11fd7995da64ce97e0306ec952d76e43c849a4ee
t4sk <tsk.nakamura@gmail.com>
2021-10-07 14:44:28 +0900

    strategy curve factory zap

--- contracts/interfaces/curve/Deposit.sol
+++ contracts/interfaces/curve/Deposit.sol
@@ -4,6 +4,7 @@ pragma solidity 0.7.6;
 interface Deposit {
     function token() external view returns (address);
 
+    // zaps //
     function add_liquidity(uint[2] calldata _amounts, uint _min_mint_amount) external;
 
     function add_liquidity(uint[3] calldata _amounts, uint _min_mint_amount) external;
@@ -11,6 +12,32 @@ interface Deposit {
     function add_liquidity(uint[4] calldata _amounts, uint _min_mint_amount) external;
 
     function remove_liquidity_one_coin(
+        uint _burn_amount,
+        int128 _i,
+        uint _min_amount
+    ) external;
+
+    // curve factory zaps //
+    function add_liquidity(
+        address _pool,
+        uint[2] calldata _amounts,
+        uint _min_mint_amount
+    ) external;
+
+    function add_liquidity(
+        address _pool,
+        uint[3] calldata _amounts,
+        uint _min_mint_amount
+    ) external;
+
+    function add_liquidity(
+        address _pool,
+        uint[4] calldata _amounts,
+        uint _min_mint_amount
+    ) external;
+
+    function remove_liquidity_one_coin(
+        address _pool,
         uint _burn_amount,
         int128 _i,
         uint _min_amount
---
+++ contracts/strategies/convex/StrategyConvexCurveFactoryZap2.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.6;
+// TODO: compiler version
+
+// version 1.0.0
+
+import "../../interfaces/curve/Deposit.sol";
+import "./StrategyConvex.sol";
+
+uint constant N_TOKENS = 2;
+
+contract StrategyConvexCurveFactoryZap2 is StrategyConvex {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint;
+
+    Deposit private immutable ZAP;
+
+    constructor(
+        address _token,
+        address _vault,
+        address _treasury,
+        address _booster,
+        uint _pid,
+        address _zap,
+        address _curve,
+        address _lp,
+        uint _index,
+        uint _decimals,
+        address[] memory _rewards,
+        address[] memory _dexes
+    )
+        StrategyConvex(
+            _token,
+            _vault,
+            _treasury,
+            _booster,
+            _pid,
+            _curve,
+            _lp,
+            _index,
+            _decimals,
+            _rewards,
+            _dexes
+        )
+    {
+        require(_index < N_TOKENS, "index >= N_TOKENS");
+
+        ZAP = Deposit(_zap);
+
+        address lp = Deposit(_zap).token();
+        require(lp == _lp, "zap lp != curve lp");
+
+        // allow token deposit into curve
+        IERC20(_token).safeApprove(_zap, type(uint).max);
+
+        // allow withdraw from curve
+        IERC20(lp).safeApprove(_zap, type(uint).max);
+    }
+
+    function _addLiquidity(uint _amount, uint _min) internal override {
+        uint[N_TOKENS] memory amounts;
+        amounts[INDEX] = _amount;
+        ZAP.add_liquidity(address(CURVE), amounts, _min);
+    }
+
+    function _removeLiquidity(uint _shares, uint _min) internal override {
+        ZAP.remove_liquidity_one_coin(address(CURVE), _shares, int128(INDEX), _min);
+    }
+}
