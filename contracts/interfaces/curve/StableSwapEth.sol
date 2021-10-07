04bb96d9046b4b1110d555f3d8e31a890d98a659
t4sk <tsk.nakamura@gmail.com>
2021-10-07 16:16:59 +0900

    archive curve interfaces

---
+++ contracts/interfaces/curve/StableSwapEth.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.6;
+
+interface StableSwapEth {
+    function get_virtual_price() external view returns (uint);
+
+    function add_liquidity(uint[2] memory amounts, uint min) external payable;
+
+    function add_liquidity(uint[3] memory amounts, uint min) external payable;
+
+    function add_liquidity(uint[4] memory amounts, uint min) external payable;
+
+    function remove_liquidity_one_coin(
+        uint _token_amount,
+        int128 i,
+        uint min_amount
+    ) external;
+}
