91d8fe9798cafc019e3fedbcfffef05b7c6b2926
t4sk <tsk.nakamura@gmail.com>
2021-10-11 16:17:28 +0900

    strategy al eth

---
+++ contracts/interfaces/curve/StableSwapAlEth.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.6;
+
+interface StableSwapAlEth {
+    function get_virtual_price() external view returns (uint);
+
+    function add_liquidity(uint[2] memory amounts, uint min) external payable;
+
+    function remove_liquidity_one_coin(
+        uint _token_amount,
+        int128 i,
+        uint min_amount
+    ) external;
+}
