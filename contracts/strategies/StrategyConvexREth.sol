a2bab9b1e5d6ff7029cf2c005d09d9cdbd782847
t4sk <tsk.nakamura@gmail.com>
2021-10-11 14:51:05 +0900

    strategy Convex Reth

---
+++ contracts/strategies/StrategyConvexREth.sol
@@ -0,0 +1,405 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.6;
+// version 0.1.1
+
+import "../interfaces/uniswap/UniswapV2Router.sol";
+import "../interfaces/convex/BaseRewardPool.sol";
+import "../interfaces/convex/Booster.sol";
+import "../interfaces/curve/StableSwapREth.sol";
+import "../StrategyEth.sol";
+
+contract StrategyConvexREth is StrategyEth {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint;
+
+    // Uniswap and Sushiswap //
+    address private constant UNISWAP = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant SUSHISWAP = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    uint private constant NUM_REWARDS = 3;
+    // address of DEX (uniswap or sushiswap) to use for selling reward tokens
+    // CRV, CVX, FIS
+    address[NUM_REWARDS] public dex;
+
+    address private constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+    address private constant CVX = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
+    address private constant FIS = 0xef3A930e1FfFFAcd2fc13434aC81bD278B0ecC8d;
+
+    // Solc 0.7 cannot create constant arrays
+    address[NUM_REWARDS] private REWARDS = [CRV, CVX, FIS];
+
+    // Convex //
+    Booster private constant BOOSTER =
+        Booster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
+    // pool id
+    uint private constant PID = 35;
+    BaseRewardPool private constant REWARD =
+        BaseRewardPool(0x61dB6c2321f784c8fAb8d5eF80f58F27C831dCc8);
+    bool public shouldClaimExtras = true;
+
+    // Curve //
+    // StableSwap rEth
+    StableSwapREth private constant CURVE_POOL =
+        StableSwapREth(0xF9440930043eb3997fc70e1339dBb11F341de7A8);
+    // LP token for curve pool (ETH / rEth)
+    IERC20 private constant CURVE_LP =
+        IERC20(0x53a901d48795C58f485cBB38df08FA96a24669D5);
+
+    // prevent slippage from deposit / withdraw
+    uint public slip = 100;
+    uint private constant SLIP_MAX = 10000;
+
+    /*
+    0 - ETH
+    1 - rEth
+    */
+    uint private constant INDEX = 0; // index of token
+
+    constructor(address _fundManager, address _treasury)
+        StrategyEth(_fundManager, _treasury)
+    {
+        (address lptoken, , , address crvRewards, , ) = BOOSTER.poolInfo(PID);
+        require(address(CURVE_LP) == lptoken, "curve pool lp != pool info lp");
+        require(address(REWARD) == crvRewards, "reward != pool info reward");
+
+        // deposit into BOOSTER
+        CURVE_LP.safeApprove(address(BOOSTER), type(uint).max);
+
+        _setDex(0, SUSHISWAP); // CRV
+        _setDex(1, SUSHISWAP); // CVX
+        _setDex(2, UNISWAP); // FIS
+    }
+
+    function _setDex(uint _i, address _dex) private {
+        IERC20 reward = IERC20(REWARDS[_i]);
+
+        // disallow previous dex
+        if (dex[_i] != address(0)) {
+            reward.safeApprove(dex[_i], 0);
+        }
+
+        dex[_i] = _dex;
+
+        // approve new dex
+        reward.safeApprove(_dex, type(uint).max);
+    }
+
+    function setDex(uint _i, address _dex) external onlyTimeLock {
+        require(_dex != address(0), "dex = 0 address");
+        _setDex(_i, _dex);
+    }
+
+    /*
+    @notice Set max slippage for deposit and withdraw from Curve pool
+    @param _slip Max amount of slippage allowed
+    */
+    function setSlip(uint _slip) external onlyAuthorized {
+        require(_slip <= SLIP_MAX, "slip > max");
+        slip = _slip;
+    }
+
+    // @dev Claim extra rewards from Convex
+    function setShouldClaimExtras(bool _shouldClaimExtras) external onlyAuthorized {
+        shouldClaimExtras = _shouldClaimExtras;
+    }
+
+    function _totalAssets() private view returns (uint total) {
+        /*
+        s0 = shares in curve pool
+        p0 = price per share of curve pool
+        a = amount of ETH
+
+        a = s0 * p0
+        */
+        // amount of Curve LP tokens in Convex
+        uint lpBal = REWARD.balanceOf(address(this));
+        // amount of ETH converted from Curve LP
+        total = lpBal.mul(CURVE_POOL.get_virtual_price()) / 1e18;
+
+        total = total.add(address(this).balance);
+    }
+
+    function totalAssets() external view override returns (uint) {
+        return _totalAssets();
+    }
+
+    function _deposit() private {
+        uint bal = address(this).balance;
+        if (bal > 0) {
+            uint[2] memory amounts;
+            amounts[INDEX] = bal;
+            /*
+            shares = ETH amount * 1e18 / price per share
+            */
+            uint pricePerShare = CURVE_POOL.get_virtual_price();
+            uint shares = bal.mul(1e18).div(pricePerShare);
+            uint min = shares.mul(SLIP_MAX - slip) / SLIP_MAX;
+
+            CURVE_POOL.add_liquidity{value: bal}(amounts, min);
+        }
+
+        uint lpBal = CURVE_LP.balanceOf(address(this));
+        if (lpBal > 0) {
+            require(BOOSTER.deposit(PID, lpBal, true), "deposit failed");
+        }
+    }
+
+    function deposit(uint _amount, uint _min) external override onlyAuthorized {
+        require(_amount > 0, "deposit = 0");
+
+        uint borrowed = fundManager.borrow(_amount);
+        require(borrowed >= _min, "borrowed < min");
+
+        _deposit();
+        emit Deposit(_amount, borrowed);
+    }
+
+    function _calcSharesToWithdraw(
+        uint _amount,
+        uint _total,
+        uint _totalShares
+    ) private pure returns (uint) {
+        /*
+        calculate shares to withdraw
+
+        a = amount of ETH to withdraw
+        T = total amount of ETH locked in external liquidity pool
+        s = shares to withdraw
+        P = total shares deposited into external liquidity pool
+
+        a / T = s / P
+        s = a / T * P
+        */
+        if (_total > 0) {
+            // avoid rounding errors and cap shares to be <= total shares
+            if (_amount >= _total) {
+                return _totalShares;
+            }
+            return _amount.mul(_totalShares) / _total;
+        }
+        return 0;
+    }
+
+    function _withdraw(uint _amount) private returns (uint) {
+        uint bal = address(this).balance;
+        if (_amount <= bal) {
+            return _amount;
+        }
+
+        uint total = _totalAssets();
+
+        if (_amount >= total) {
+            _amount = total;
+        }
+
+        uint need = _amount - bal;
+        uint totalShares = REWARD.balanceOf(address(this));
+        // total assets is always >= bal
+        uint shares = _calcSharesToWithdraw(need, total - bal, totalShares);
+
+        // withdraw from Convex
+        if (shares > 0) {
+            // true = claim CRV
+            require(REWARD.withdrawAndUnwrap(shares, false), "reward withdraw failed");
+        }
+
+        // withdraw from Curve
+        uint lpBal = CURVE_LP.balanceOf(address(this));
+        if (shares > lpBal) {
+            shares = lpBal;
+        }
+
+        if (shares > 0) {
+            uint min = need.mul(SLIP_MAX - slip) / SLIP_MAX;
+            CURVE_POOL.remove_liquidity_one_coin(shares, int128(INDEX), min);
+        }
+
+        uint balAfter = address(this).balance;
+        if (balAfter < _amount) {
+            return balAfter;
+        }
+        // balAfter >= _amount >= total
+        // requested to withdraw all so return balAfter
+        if (_amount >= total) {
+            return balAfter;
+        }
+        // requested withdraw < all
+        return _amount;
+    }
+
+    function withdraw(uint _amount)
+        external
+        override
+        onlyFundManager
+        returns (uint loss)
+    {
+        require(_amount > 0, "withdraw = 0");
+
+        // availabe <= _amount
+        uint available = _withdraw(_amount);
+
+        uint debt = fundManager.getDebt(address(this));
+        uint total = _totalAssets();
+        if (debt > total) {
+            loss = debt - total;
+        }
+
+        if (available > 0) {
+            _sendEth(msg.sender, available);
+        }
+
+        emit Withdraw(_amount, available, loss);
+    }
+
+    function repay(uint _amount, uint _min) external override onlyAuthorized {
+        require(_amount > 0, "repay = 0");
+        // availabe <= _amount
+        uint available = _withdraw(_amount);
+        uint repaid = fundManager.repay{value: available}(available);
+        require(repaid >= _min, "repaid < min");
+
+        emit Repay(_amount, repaid);
+    }
+
+    /*
+    @dev Uniswap fails with zero address so no check is necessary here
+    */
+    function _swap(
+        address _dex,
+        address _tokenIn,
+        uint _amount
+    ) private {
+        // create dynamic array with 2 elements
+        address[] memory path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = WETH;
+
+        UniswapV2Router(_dex).swapExactTokensForETH(
+            _amount,
+            1,
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function _claimRewards(uint _minProfit) private {
+        // calculate profit = balance of ETH after - balance of ETH before
+        uint diff = address(this).balance;
+
+        require(
+            REWARD.getReward(address(this), shouldClaimExtras),
+            "get reward failed"
+        );
+
+        for (uint i = 0; i < NUM_REWARDS; i++) {
+            uint rewardBal = IERC20(REWARDS[i]).balanceOf(address(this));
+            if (rewardBal > 0) {
+                _swap(dex[i], REWARDS[i], rewardBal);
+            }
+        }
+
+        diff = address(this).balance - diff;
+        require(diff >= _minProfit, "profit < min");
+
+        // transfer performance fee to treasury
+        if (diff > 0) {
+            uint fee = diff.mul(perfFee) / PERF_FEE_MAX;
+            if (fee > 0) {
+                _sendEth(treasury, fee);
+                diff = diff.sub(fee);
+            }
+        }
+
+        emit ClaimRewards(diff);
+    }
+
+    function claimRewards(uint _minProfit) external override onlyAuthorized {
+        _claimRewards(_minProfit);
+    }
+
+    function _skim() private {
+        uint total = _totalAssets();
+        uint debt = fundManager.getDebt(address(this));
+        require(total > debt, "total <= debt");
+
+        uint profit = total - debt;
+        // reassign to actual amount withdrawn
+        profit = _withdraw(profit);
+
+        emit Skim(total, debt, profit);
+    }
+
+    function skim() external override onlyAuthorized {
+        _skim();
+    }
+
+    function _report(uint _minTotal, uint _maxTotal) private {
+        uint total = _totalAssets();
+        require(total >= _minTotal, "total < min");
+        require(total <= _maxTotal, "total > max");
+
+        uint gain = 0;
+        uint loss = 0;
+        uint free = 0; // balance of ETH
+        uint debt = fundManager.getDebt(address(this));
+        if (total > debt) {
+            gain = total - debt;
+
+            free = address(this).balance;
+            if (gain > free) {
+                gain = free;
+            }
+        } else {
+            loss = debt - total;
+        }
+
+        if (gain > 0 || loss > 0) {
+            fundManager.report{value: gain}(gain, loss);
+        }
+
+        emit Report(gain, loss, free, total, debt);
+    }
+
+    function report(uint _minTotal, uint _maxTotal) external override onlyAuthorized {
+        _report(_minTotal, _maxTotal);
+    }
+
+    function harvest(
+        uint _minProfit,
+        uint _minTotal,
+        uint _maxTotal
+    ) external override onlyAuthorized {
+        _claimRewards(_minProfit);
+        _skim();
+        _report(_minTotal, _maxTotal);
+    }
+
+    function migrate(address payable _strategy) external override onlyFundManager {
+        StrategyEth strat = StrategyEth(_strategy);
+        require(address(strat.token()) == ETH, "strategy token != ETH");
+        require(
+            address(strat.fundManager()) == address(fundManager),
+            "strategy fund manager != fund manager"
+        );
+
+        if (claimRewardsOnMigrate) {
+            _claimRewards(1);
+        }
+
+        uint bal = _withdraw(type(uint).max);
+        // WARNING: may lose all ETH if sent to wrong address
+        _sendEth(address(strat), bal);
+    }
+
+    /*
+    @notice Transfer token accidentally sent here to admin
+    @param _token Address of token to transfer
+    */
+    function sweep(address _token) external override onlyAuthorized {
+        for (uint i = 0; i < NUM_REWARDS; i++) {
+            require(_token != REWARDS[i], "protected token");
+        }
+        IERC20(_token).safeTransfer(admin, IERC20(_token).balanceOf(address(this)));
+    }
+}
